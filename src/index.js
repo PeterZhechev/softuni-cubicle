92b1788586a2eb2b43196f52c8035d0bd225c27f
Peter Zhechev <118084167+PeterZhechev@users.noreply.github.com>
2024-06-13 21:19:19 +0300

    Get cubes from db

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ handlebarsConfig(app);
 dbConnect()
     .then(() => console.log('DB connected successfully'))
     .catch(err => {
-        console.log('DB error: ', err);
+        console.log('DB error: ', err.message);
     });
 
 app.use(routes);
